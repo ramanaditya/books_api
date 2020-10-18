3abcb227d3419b51e9face2809ae319c42479242
ramanaditya <adityaraman96@gmail.com>
2020-10-18 13:34:50 +0530

    Google Book Test Case

--- tests/products/googlebooks/tests.py
+++ tests/products/googlebooks/tests.py
@@ -18,19 +18,22 @@ class GoogleBooksAPITest(APITestCase):
         self.token = Token.objects.create(user=self.user)
         self.token.save()
 
-    @vcr.use_cassette(
-        "books_api/products/googlebooks/test-googlebooksapi.yml",
-        filter_query_parameters=["q"],
-    )
     def test_googlebooks_api(self):
-        factory = APIRequestFactory()
-        view = GoogleBooksViewSet.as_view({"get": "list"})
-        request = factory.get(
-            "/googlebooks/?format=json&q=None",
-            HTTP_AUTHORIZATION=f"Token {self.token}",
-            format="json",
-        )
-        response = view(request)
-        self.assertEqual(
-            response.status_code, 200, "Response error: {}".format(response.data)
-        )
+        with vcr.use_cassette(
+            "data/products/googlebooks/test-googlebooks.yaml",
+            serializer="yaml",
+            record_mode="once",
+            filter_query_parameters=["q"],
+            match_on=("body",),
+        ):
+            factory = APIRequestFactory()
+            view = GoogleBooksViewSet.as_view({"get": "list"})
+            request = factory.get(
+                "/googlebooks/?format=json&q=None",
+                HTTP_AUTHORIZATION=f"Token {self.token}",
+                format="yaml",
+            )
+            response = view(request)
+            self.assertEqual(
+                response.status_code, 200, "Response error: {}".format(response.data)
+            )
