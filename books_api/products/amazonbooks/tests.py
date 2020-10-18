6fb78324a890221e6e6bbde81b198a5f2a957d43
ramanaditya <adityaraman96@gmail.com>
2020-10-18 07:16:38 +0530

    Amazon API test case improved

--- books_api/products/amazonbooks/tests.py
+++ books_api/products/amazonbooks/tests.py
@@ -1,10 +1,34 @@
-import requests
 import vcr
-from django.conf import settings
+from rest_framework.authtoken.models import Token
+from rest_framework.test import APIRequestFactory
+from rest_framework.test import APITestCase
+
+from books_api.products.googlebooks.api.views import GoogleBooksViewSet
+from books_api.users.models import User
 
 
-@vcr.use_cassette("books_api/products/amazonbooks/test-amazonbooksapi.yml")
-def test_amazonbooks_api():
-    url = f"{settings.AMAZON_BOOKS_API}/s"
-    response = requests.get(f"{url}", headers=settings.HEADERS_MAC,)
-    assert response.status_code == 200
+class AmazonBooksAPITest(APITestCase):
+    def setUp(self):
+        self.factory = APIRequestFactory()
+        self.user = User.objects.create_user(
+            username="user@foo.com", email="user@foo.com", password="top_secret"
+        )
+        self.token = Token.objects.create(user=self.user)
+        self.token.save()
+
+    @vcr.use_cassette(
+        "books_api/products/amazonbooks/test-amazonbooksapi.yml",
+        filter_query_parameters=["q"],
+    )
+    def test_googlebooks_api(self):
+        factory = APIRequestFactory()
+        view = GoogleBooksViewSet.as_view({"get": "list"})
+        request = factory.get(
+            "/amazonbooks/?format=json&q=None",
+            HTTP_AUTHORIZATION=f"Token {self.token}",
+            format="json",
+        )
+        response = view(request)
+        self.assertEqual(
+            response.status_code, 200, "Response error: {}".format(response.data)
+        )
