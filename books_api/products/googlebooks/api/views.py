e75bd0a03511f5869b95a8f8687a8c9b29fe8515
ramanaditya <adityaraman96@gmail.com>
2020-10-19 12:23:48 +0530

    Improved views

--- books_api/products/googlebooks/api/views.py
+++ books_api/products/googlebooks/api/views.py
@@ -6,21 +6,27 @@ from rest_framework import viewsets
 from rest_framework.response import Response
 
 
-def getCustomizedList(items):
+def getCustomizedList(output):
     ret = list()
-    for item in items:
-        data = dict()
-        vol_info = item.get("volumeInfo", None)
-        if vol_info:
-            # title contains the title of the book
-            # identifiers contains the isbn details
-            data["title"] = vol_info.get("title", None)
-            identifiers = vol_info.get("industryIdentifiers", None)
-            if identifiers:
-                for identifier in identifiers:
-                    data[identifier["type"]] = identifier["identifier"]
-        ret.append(data)
-    return ret
+    items = list()
+    try:
+        items = output["items"]
+    except:
+        items = items
+    if items:
+        for item in items:
+            data = dict()
+            vol_info = item.get("volumeInfo", None)
+            if vol_info:
+                # title contains the title of the book
+                # identifiers contains the isbn details
+                data["title"] = vol_info.get("title", None)
+                identifiers = vol_info.get("industryIdentifiers", None)
+                if identifiers:
+                    for identifier in identifiers:
+                        data[identifier["type"]] = identifier["identifier"]
+            ret.append(data)
+        return ret
 
 
 class GoogleBooksViewSet(viewsets.ViewSet):
@@ -39,7 +45,5 @@ class GoogleBooksViewSet(viewsets.ViewSet):
         ret = list()
         if response.status_code == 200:
             output = json.loads(response.text)
-            items = output.get("items", None)
-            if items:
-                ret = getCustomizedList(items)
+            ret = getCustomizedList(output)
         return Response(ret)
