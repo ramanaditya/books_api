2967d2ac6f90053bbf49ed922b1e08f59476ba92
ramanaditya <adityaraman96@gmail.com>
2020-10-19 22:57:57 +0530

    Test case reformatted for googlebooks

--- tests/products/googlebooks/tests.py
+++ tests/products/googlebooks/tests.py
@@ -1,4 +1,6 @@
 import json
+import os
+import time
 
 import vcr
 from rest_framework.authtoken.models import Token
@@ -12,16 +14,21 @@ from books_api.products.googlebooks.api.views import (
 from books_api.users.models import User
 
 
-def reformat_response_body():
-    def before_record_response(response):
-        items = json.loads(response["body"]["string"])
-        data = list()
-        if items:
-            data = getCustomizedList(items)
-        response["body"]["output"] = data
-        return response
+def save_googlebooks_output(file_name):
+    data = dict()
+    result = list()
+    with open(file_name, "r") as f:
+        result = json.load(f)
+    f.close()
+    items = json.loads(result["interactions"][0]["response"]["body"]["string"])
+    if items:
+        data = getCustomizedList(items)
 
-    return before_record_response
+        dir_list = file_name.split("/")
+        out_file_name = f'data/{"/".join(dir_list[1:])}'
+        with open(out_file_name, "w") as outfile:
+            json.dump(data, outfile, ensure_ascii=False, indent=4)
+        outfile.close()
 
 
 class GoogleBooksAPITest(APITestCase):
@@ -36,22 +43,34 @@ class GoogleBooksAPITest(APITestCase):
         self.view = GoogleBooksViewSet.as_view({"get": "list"})
 
     def test_googlebooks_api(self):
+        query_string = "The Alchemist"
+        file_name = (
+            f"vcr_py/products/googlebooks/{''.join(query_string.split(' '))}.json"
+        )
         with vcr.use_cassette(
-            "data/products/googlebooks/test-googlebooks.json",
+            f"{file_name}",
             serializer="json",
             record_mode="once",
             filter_query_parameters=["q"],
             match_on=("body",),
             decode_compressed_response=True,
-            before_record_response=reformat_response_body(),
+            # before_record_response=save_googlebooks_output(file_name),
         ):
             request = self.factory.get(
-                "/googlebooks/?format=json&q=None",
+                f"/googlebooks/?format=json&q={query_string}",
                 HTTP_AUTHORIZATION=f"Token {self.token}",
                 format="json",
             )
             response = self.view(request)
-            response.render()
             self.assertEqual(
                 response.status_code, 200, "Response error: {}".format(response.data),
             )
+
+        max_check = 10
+        for i in range(max_check):
+            try:
+                with open(file_name, "rb") as _:
+                    break
+            except IOError:
+                time.sleep(3)
+        save_googlebooks_output(file_name)
