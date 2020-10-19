0384ba4a5f24cd81d2d5f8386dd3b60e7c056310
ramanaditya <adityaraman96@gmail.com>
2020-10-19 11:19:45 +0530

    Made the code modular by using function which is further used in the test

--- books_api/products/googlebooks/api/views.py
+++ books_api/products/googlebooks/api/views.py
@@ -6,6 +6,23 @@ from rest_framework import viewsets
 from rest_framework.response import Response
 
 
+def getCustomizedList(items):
+    ret = list()
+    for item in items:
+        data = dict()
+        vol_info = item.get("volumeInfo", None)
+        if vol_info:
+            # title contains the title of the book
+            # identifiers contains the isbn details
+            data["title"] = vol_info.get("title", None)
+            identifiers = vol_info.get("industryIdentifiers", None)
+            if identifiers:
+                for identifier in identifiers:
+                    data[identifier["type"]] = identifier["identifier"]
+        ret.append(data)
+    return ret
+
+
 class GoogleBooksViewSet(viewsets.ViewSet):
     def list(self, request, *args, **kwargs):
         """
@@ -24,16 +41,5 @@ class GoogleBooksViewSet(viewsets.ViewSet):
             output = json.loads(response.text)
             items = output.get("items", None)
             if items:
-                for item in items:
-                    data = dict()
-                    vol_info = item.get("volumeInfo", None)
-                    if vol_info:
-                        # title contains the title of the book
-                        # identifiers contains the isbn details
-                        data["title"] = vol_info.get("title", None)
-                        identifiers = vol_info.get("industryIdentifiers", None)
-                        if identifiers:
-                            for identifier in identifiers:
-                                data[identifier["type"]] = identifier["identifier"]
-                    ret.append(data)
+                ret = getCustomizedList(items)
         return Response(ret)
