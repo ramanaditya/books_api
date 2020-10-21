86bd0ddb7c5c37bc19dea3e36a583e1fb5448655
ramanaditya <adityaraman96@gmail.com>
2020-10-21 18:23:12 +0530

    Updated test case and ouput serializer to yaml

--- tests/products/googlebooks/tests.py
+++ tests/products/googlebooks/tests.py
@@ -1,35 +1,24 @@
 import json
+import time
 
 import vcr
+from django.conf import settings
 from rest_framework.authtoken.models import Token
 from rest_framework.test import APIRequestFactory
 from rest_framework.test import APITestCase
 
-from books_api.products.googlebooks.api.views import (
-    GoogleBooksViewSet,
-    getCustomizedList,
-)
+from books_api.products.googlebooks.api.views import GoogleBooksViewSet
 from books_api.users.models import User
 
 
 def write_data_to_json(file_name, result):
     dir_list = file_name.split("/")
-    out_file_name = f'data/{"/".join(dir_list[1:])}'
+    out_file_name = (
+        f'data/{"/".join(dir_list[1:-1])}/{dir_list[-1].replace("yaml", "json")}'
+    )
     with open(out_file_name, "w") as outfile:
         json.dump(result, outfile, ensure_ascii=False, indent=4)
     outfile.close()
-
-
-def get_googlebooks_output(file_name):
-    data = dict()
-    result = list()
-    with open(file_name, "r") as f:
-        result = json.load(f)
-    f.close()
-    items = json.loads(result["interactions"][0]["response"]["body"]["string"])
-    if items:
-        data = getCustomizedList(items)
-    return data
 
 
 class GoogleBooksAPITest(APITestCase):
@@ -44,13 +33,13 @@ class GoogleBooksAPITest(APITestCase):
         self.view = GoogleBooksViewSet.as_view({"get": "list"})
         self.query_string = "The Alchemist"
         self.file_name = (
-            f"vcr_py/products/googlebooks/{''.join(self.query_string.split(' '))}.json"
+            f"vcr_py/products/googlebooks/{''.join(self.query_string.split(' '))}.yaml"
         )
 
     def test_googlebooks_api(self):
         with vcr.use_cassette(
             f"{self.file_name}",
-            serializer="json",
+            serializer="yaml",
             record_mode="once",
             filter_query_parameters=["q"],
             match_on=("body",),
@@ -63,17 +52,24 @@ class GoogleBooksAPITest(APITestCase):
             )
             response = self.view(request)
             response.render()
-            write_data_to_json(self.file_name, response.data)
-            self.assertEqual(
-                response.status_code, 200, "Response error: {}".format(response.data),
-            )
-
-    def test_vcr_and_viewsets_googlebooks(self):
-        saved_data = list()
-        dirs_list = self.file_name.split("/")
-        data_file_name = f"data/{'/'.join(dirs_list[1:])}"
-        with open(data_file_name, "r") as infile:
-            saved_data = json.load(infile)
-        infile.close()
-        vcr_output = get_googlebooks_output(self.file_name)
-        assert saved_data == vcr_output
+            vcr_output = response.data
+            if settings.VCR_WRITE_MODE:
+                max_check = 10
+                for i in range(max_check):
+                    try:
+                        with open(self.file_name, "rb") as _:
+                            break
+                    except IOError:
+                        time.sleep(3)
+                write_data_to_json(self.file_name, response.data)
+                self.assertEqual(
+                    response.status_code, 200, "Response error: {}".format(response)
+                )
+            else:
+                saved_data = list()
+                dirs_list = self.file_name.split("/")
+                data_file_name = f"data/{'/'.join(dirs_list[1:-1])}/{dirs_list[-1].replace('yaml','json')}"
+                with open(data_file_name, "r") as infile:
+                    saved_data = json.load(infile)
+                infile.close()
+                assert vcr_output == saved_data
